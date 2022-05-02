79d7045ead14626f68a0197535fb6b838e21ee14
Maxime TFT <mkubik@tftlabs.com>
2022-05-02 15:09:23 +0200

    💎 feat: start of learning threejs - add: threejs lib + add nextjs transpile module to deal with threejs/drei/preprocessor

--- components/Footer.js
+++ components/Footer.js
@@ -14,7 +14,7 @@ export default function Footer() {
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
         </div>
-        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+        <div className="mb-8 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,6 +3,7 @@ const headerNavLinks = [
   // { href: '/learning', title: 'Learning' },
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
+  { href: '/sandbox', title: 'Sandbox' },
   // { href: '/tags', title: 'Tags' },
   { href: '/about', title: 'About' },
 ]
