f61742d7c28a5dd710d7d146c32848487fda91ed
Maxime Kubik <mkubdev@gmail.com>
2022-05-17 14:39:18 +0200

    [ui] add company link to authorLayout

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -25,7 +25,9 @@ export default function AuthorLayout({ children, frontMatter }) {
             />
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
+            <div className="text-purple-500 hover:text-purple-500 dark:hover:text-purple-400">
+              <a href="https://tftlabs.fr/">{company}</a>
+            </div>
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
