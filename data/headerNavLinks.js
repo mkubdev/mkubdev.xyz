2cc6766f9ed01e8ffa36ec115cb0839624681a93
Max Kubik <50140834+mkubdev@users.noreply.github.com>
2022-05-24 13:52:54 +0200

    Update headerNavLinks.js
    remove Sandbox from the menu till i add threejs series

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,7 +3,7 @@ const headerNavLinks = [
   // { href: '/learning', title: 'Learning' },
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
-  { href: '/sandbox', title: 'Sandbox' },
+  // { href: '/sandbox', title: 'Sandbox' },
   // { href: '/tags', title: 'Tags' },
   { href: '/about', title: 'About' },
 ]
