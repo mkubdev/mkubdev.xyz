7e6ae4de2513d2e620864cab8a7074417ed09771
Maxime TFT <mkubik@tftlabs.com>
2022-07-18 15:57:53 +0200

    fix: remove social icon not displaying on chrome (need fix asap!)

--- components/Footer.js
+++ components/Footer.js
@@ -7,14 +7,15 @@ export default function Footer() {
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center">
-        <div className="mb-3 flex space-x-4">
+        {/* TODO: fix social icon */}
+        {/* <div className="mb-3 flex space-x-4">
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
           <SocialIcon kind="github" href={siteMetadata.github} size="6" />
           <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
           <SocialIcon kind="youtube" href={siteMetadata.youtube} size="6" />
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
-        </div>
+        </div> */}
         <div className="mb-3 flex">
           <NowPlaying />
         </div>
